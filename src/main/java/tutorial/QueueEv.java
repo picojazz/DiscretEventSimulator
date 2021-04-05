b70d7365e32767ec7b663a051b8647001de4a0bb
picojazz <picojazzz@gmail.com>
2021-04-05 16:23:57 +0000

    system N

--- src/main/java/tutorial/QueueEv.java
+++ src/main/java/tutorial/QueueEv.java
@@ -10,11 +10,19 @@ public class QueueEv {
    RandomVariateGen genArr;
    RandomVariateGen genServ;
    LinkedList<Customer> waitList = new LinkedList<Customer> ();
+   LinkedList<Customer> waitList2 = new LinkedList<Customer> ();
    LinkedList<Customer> servList = new LinkedList<Customer> ();
+   LinkedList<Customer> servList2 = new LinkedList<Customer> ();
    Tally custWaits     = new Tally ("Waiting times");
    Accumulate totWait  = new Accumulate ("Size of queue");
    
-   class Customer { double arrivTime, servTime; }
+   class Customer {
+      double arrivTime, servTime;
+      int type;
+      public Customer(){
+         type = (int)(1 + Math.random() * ( 3 - 1 ));
+      }
+   }
 
    public QueueEv (double lambda, double mu) {
       genArr = new ExponentialGen (new MRG32k3a(), lambda);
@@ -30,32 +38,97 @@ public class QueueEv {
    }
 
    class Arrival extends Event {
+
+      public void addCustToServ(LinkedList<Customer> serv,Customer cust){
+            custWaits.add (0.0);
+            serv.addLast (cust);
+      }
+      public void addCustToWait(LinkedList<Customer> wait,Customer cust){
+         wait.addLast (cust);
+         totWait.update (wait.size());
+      }
+
+
       public void actions() {
          new Arrival().schedule (genArr.nextDouble()); // Next arrival.
          Customer cust = new Customer();  // Cust just arrived.
          cust.arrivTime = Sim.time();
          cust.servTime = genServ.nextDouble();
-         if (servList.size() > 0) {       // Must join the queue.
-            waitList.addLast (cust);
-            totWait.update (waitList.size());
-         } else {                         // Starts service.
-            custWaits.add (0.0);
-            servList.addLast (cust);
-            new Departure().schedule (cust.servTime);
+         //routing for a customer type 1
+         if(cust.type == 1){
+            if(servList.size() == 0){
+               addCustToServ(servList,cust);
+               new Departure(cust,1).schedule (cust.servTime);
+            }else if(servList2.size() == 0){
+               addCustToServ(servList2,cust);
+               new Departure(cust,2).schedule (cust.servTime);
+            }else {
+               addCustToWait(waitList,cust);
+
+            }
          }
+         if(cust.type == 2){
+            if (servList2.size() > 0) {       // Must join the queue.
+               addCustToWait(waitList2,cust);
+            } else {                         // Starts service.
+               addCustToServ(servList2,cust);
+               new Departure(cust,2).schedule (cust.servTime);
+            }
+         }
+
       }
    }
 
    class Departure extends Event {
+      int type,serv;
+
+      public Departure(Customer cust, int serv ){
+         type = cust.type;
+         this.serv = serv;
+      }
+
+      public void addCustFromWaitToServ1(LinkedList<Customer> serv,LinkedList<Customer> wait){
+         Customer cust = wait.removeFirst();
+         totWait.update(wait.size());
+         custWaits.add(Sim.time() - cust.arrivTime);
+         serv.addLast(cust);
+         new Departure(cust, 1).schedule(cust.servTime);
+      }
+      public void addCustFromWaitToServ2(LinkedList<Customer> serv,LinkedList<Customer> wait){
+         Customer cust = wait.removeFirst();
+         totWait.update(wait.size());
+         custWaits.add(Sim.time() - cust.arrivTime);
+         serv.addLast(cust);
+         new Departure(cust, 2).schedule(cust.servTime);
+      }
+
       public void actions() {
-         servList.removeFirst();
-         if (waitList.size() > 0) {
-            // Starts service for next one in queue.
-            Customer cust = waitList.removeFirst();
-            totWait.update (waitList.size());
-            custWaits.add (Sim.time() - cust.arrivTime);
-            servList.addLast (cust);
-            new Departure().schedule (cust.servTime);
+         if (type == 1) {
+            if (serv == 1) {
+               servList.removeFirst();
+               if (waitList.size() > 0) {
+                  // Starts service for next one in queue.
+                  addCustFromWaitToServ1(servList,waitList);
+
+               }
+            } else {
+               servList2.removeFirst();
+               if (waitList.size() > 0) {
+                  // Starts service for next one in queue.
+                  addCustFromWaitToServ2(servList2,waitList);
+               } else if (waitList2.size() > 0) {
+                  addCustFromWaitToServ2(servList2,waitList2);
+               }
+            }
+
+         } else {
+            servList2.removeFirst();
+            if (waitList2.size() > 0) {
+               // Starts service for next one in queue.
+               addCustFromWaitToServ2(servList2,waitList2);
+            }
+
+
          }
       }
    }
